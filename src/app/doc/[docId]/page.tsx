365d98d5bcf25ee967fa6b243cabc38def5c8c5d
Wendetton <feazegoncalves@gmail.com>
2025-08-05 12:38:07 -0300

    Update page.tsx

--- src/app/doc/[docId]/page.tsx
+++ src/app/doc/[docId]/page.tsx
@@ -3,19 +3,19 @@
 import { useEffect } from 'react'
 import { useRouter } from 'next/navigation'
 
-export default function Page({ params }: { params: { id: string } }) {
+export default function Page({ params }: { params: { docId: string } }) {
   const router = useRouter()
 
   useEffect(() => {
     const perfil = localStorage.getItem('perfil-eyenote')
     if (!perfil) {
-      router.replace(`/doc/${params.id}/seletor-perfil`)
+      router.replace(`/doc/${params.docId}/seletor-perfil`)
     } else if (perfil === 'medico') {
-      router.replace(`/doc/${params.id}/medico`)
+      router.replace(`/doc/${params.docId}/medico`)
     } else {
-      router.replace(`/doc/${params.id}/assistente`)
+      router.replace(`/doc/${params.docId}/assistente`)
     }
-  }, [params.id, router])
+  }, [params.docId, router])
 
   return null
 }
