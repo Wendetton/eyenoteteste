b6c85c2eabea555ba9ca7602406ba13683e55f46
Wendetton <feazegoncalves@gmail.com>
2025-08-07 19:50:21 -0300

    Delete tailwind.config.ts

--- tailwind.config.ts
+++
@@ -1,9 +0,0 @@
-import { Config } from 'tailwindcss';
-
-const config: Config = {
-  content: ["./src/app/**/*.{ts,tsx}", "./src/components/**/*.{ts,tsx}"],
-  theme: { extend: {} },
-  plugins: [],
-};
-
-export default config;
